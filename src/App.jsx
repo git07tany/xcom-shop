e969427d315625c1ada08377a249c607597c04d2
qoqmee <l.sharganovaa@ya.ru>
2025-12-17 04:53:57 +0300

    L-2

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import CatalogPage from './pages/CatalogPage';
 import NotebooksPage from './pages/NotebooksPage';
+import SmartphonesPage from './pages/SmartphonesPage';
+import MonitorsPage from './pages/MonitorsPage';
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
             <Route path="/" element={<HomePage />} /> 
             <Route path="/catalog/notebooks" element={<NotebooksPage />} />
             <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/smartphones" element={<SmartphonesPage />} />
+            <Route path="/catalog/monitors" element={<MonitorsPage />} />
           </Routes>
         </main>
         <Footer/>
